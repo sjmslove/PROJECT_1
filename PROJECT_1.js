ff76d5494cdfbaa30c25c1874be6a9d6e269a979
sjmslove <sjsmith8147@gmail.com>
2016-10-03 16:05:03 -0400

    Change Formatting

--- PROJECT_1.js
+++ PROJECT_1.js
@@ -9,7 +9,10 @@
 const PROMPT = require('readline-sync');
 
 let  numBathrooms, numBedrooms, numCars, numTotal;
-const basePrice = 50000, baseBedroom = 17000, baseBathroom = 12500, baseCars = 6000;
+const basePrice = 50000,
+    baseBedroom = 17000,
+    baseBathroom = 12500,
+    baseCars = 6000;
 
 function main(){
     setNumBathrooms();
@@ -24,16 +27,20 @@ main()
 function setNumBathrooms (){
     numBathrooms = PROMPT.question('\n How many Bathrooms?');
 }
+
 function setNumBedrooms (){
     numBedrooms = PROMPT.question('\n How many Bedrooms?');
 }
+
 function setNumCars(){
     numCars = PROMPT.question ('\n How Many Cars in the Garage?');
 }
+
 function setnumTotal(){
 
     numTotal = basePrice + (baseBedroom * numBedrooms) + (baseBathroom * numBathrooms) + ( baseCars * numCars)
 }
+
 function printnumTotal() {
     console.log(` \n The total price is $${numTotal}`);
 }
